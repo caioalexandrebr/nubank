4bb3e75eaba49dd4dc808f69b2f6491eb363f4b6
caioalexandrebr <contato@caioalexandre.com>
2020-10-17 20:36:40 -0300

    feat(Header): add header component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
+
 import Main from './pages/Main';
 
-const App = () => <Main />;
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#8b10ab" />
+    <Main />
+  </>
+);
 
 export default App;
