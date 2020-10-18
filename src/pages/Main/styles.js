eef3bd5e62db234b81f1006dd608e5e0f2064977
caioalexandrebr <contato@caioalexandre.com>
2020-10-18 13:12:53 -0300

    styles(Card): add styles in the card component

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -2,10 +2,10 @@ import styled from 'styled-components/native';
 import {getStatusBarHeight} from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
-  flex: 1;
   background: #8b10ab;
-  padding-top: ${getStatusBarHeight}px;
+  flex: 1;
   justify-content: center;
+  padding-top: ${getStatusBarHeight}px;
 `;
 
 export const Content = styled.View`
@@ -15,30 +15,45 @@ export const Content = styled.View`
 `;
 
 export const Card = styled.View`
-  flex: 1;
   background: #fff;
   border-radius: 4px;
-  margin: 0 20px;
+  flex: 1;
   height: 100%;
+  margin: 0 20px;
 `;
 
 export const CardHeader = styled.View`
+  align-items: center;
   flex-direction: row;
   justify-content: space-between;
-  align-items: center;
   padding: 30px;
 `;
 
 export const CardContent = styled.View`
   flex: 1;
-  padding: 0 30px;
   justify-content: center;
+  padding: 0 30px;
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  color: #999;
+  font-size: 14px;
+`;
 
-export const Description = styled.Text``;
+export const Description = styled.Text`
+  color: #333;
+  font-size: 32px;
+  margin-top: 4px;
+`;
 
-export const CardFooter = styled.View``;
+export const CardFooter = styled.View`
+  background: #eee;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  padding: 30px;
+`;
 
-export const Annotation = styled.Text``;
+export const Annotation = styled.Text`
+  color: #333;
+  font-size: 12px;
+`;
