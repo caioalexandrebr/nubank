3c819a86c9bf3f508d6d1bc44dd73705b81c18e1
caioalexandrebr <contato@caioalexandre.com>
2020-10-17 22:03:18 -0300

    feat(Card): add card component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '../../components/Header';
-
 import Tabs from '../../components/Tabs';
 
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 482.246,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 150,00 recebida de Matheus Henrique hoje às
+              21:30h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
