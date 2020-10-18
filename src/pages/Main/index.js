1e5955378a90f27460cca3311cee0a1eff34e8c2
caioalexandrebr <contato@caioalexandre.com>
2020-10-17 21:21:47 -0300

    feat(Tabs): add tabs component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
+import Tabs from '../../components/Tabs';
+
 import {Container} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Tabs />
     </Container>
   );
 }
